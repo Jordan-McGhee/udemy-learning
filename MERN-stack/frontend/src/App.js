a646be557cc33de64257b9e3b7e6189ac1428cf0
Jordan McGhee <jordanmcghee95@gmail.com>
2021-11-04 22:42:07 -0400

    Added navbar and main header. Also added hamburger menu and change of state to allow hamburger to open and close

--- MERN-stack/frontend/src/App.js
+++ MERN-stack/frontend/src/App.js
@@ -2,26 +2,30 @@ import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
 
-        {/* users route */}
-        <Route path="/" exact>
-          <Users />
-        </Route>
+          {/* users route */}
+          <Route path="/" exact>
+            <Users />
+          </Route>
 
-        {/* NewPlace route */}
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
+          {/* NewPlace route */}
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
 
-        {/* redirects if none of the routes are matched */}
-        <Redirect to="/" />
-        
-      </Switch>
+          {/* redirects if none of the routes are matched */}
+          <Redirect to="/" />
+
+        </Switch>
+      </main>
     </Router>
   );
 }
