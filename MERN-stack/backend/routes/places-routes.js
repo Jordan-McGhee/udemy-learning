2b1975fac2fccb2f69bfda8b80779877eceaa740
Jordan McGhee <jordanmcghee95@gmail.com>
2021-11-22 22:44:25 -0500

    Added ability to take in post request on places routes

--- MERN-stack/backend/routes/places-routes.js
+++ MERN-stack/backend/routes/places-routes.js
@@ -8,4 +8,6 @@ router.get('/:placeID', placesControllers.getPlaceById)
 
 router.get("/user/:userID", placesControllers.getPlaceByUserId)
 
+router.post("/", placesControllers.createPlace)
+
 module.exports = router;
