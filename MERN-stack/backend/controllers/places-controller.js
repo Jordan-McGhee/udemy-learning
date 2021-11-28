9499c9a682016e57954616a51b3fa9f3131afcc5
Jordan McGhee <jordanmcghee95@gmail.com>
2021-11-28 13:55:16 -0500

    Updated user methods with mongoose/mongoDB methods to communicate with database

--- MERN-stack/backend/controllers/places-controller.js
+++ MERN-stack/backend/controllers/places-controller.js
@@ -112,8 +112,9 @@ const createPlace = async (req, res, next) => {
 
     if (!errors.isEmpty()) {
         console.log(errors)
-
-        throw new HttpError("Invalid inputs passed, please check your data.", 422)
+        return next(
+            new HttpError("Invalid inputs passed, please check your data.", 422)
+        )
     }
     
     const { title, description, address, creator } = req.body
@@ -173,8 +174,9 @@ const updatePlace = async (req, res, next) => {
 
     if (!errors.isEmpty()) {
         console.log(errors)
-        
-        throw new HttpError("Invalid inputs passed, please check your data.", 422)
+        return next(
+            new HttpError("Invalid inputs passed, please check your data.", 422)
+        )
     }
 
     const { title, description } = req.body
--- MERN-stack/backend/controllers/users-controller.js
+++ MERN-stack/backend/controllers/users-controller.js
@@ -1,6 +1,9 @@
 const uuid = require("uuid").v4
 const { validationResult } = require("express-validator")
 
+const User = require("../models/user")
+const HttpError = require("../models/http-error")
+
 const DUMMY_USERS = [
     {
         id: "u1",
@@ -17,54 +20,156 @@ const DUMMY_USERS = [
     },
 ]
 
-const HttpError = require("../models/http-error")
+const getUserList = async (req, res, next) => {
+    
+    let users
 
-const getUserList = ((req, res, next) => {
-    res.json({ users : DUMMY_USERS })
-})
+    try {
+        users = await User.find({}, "-password")
+    } catch (err) {
+        const error = new HttpError(
+            "Could not get list of users. Please try again.",
+            500
+        )
 
-const signUp = ((req, res, next) => {
+        return next(error)
+    }
+
+    res.json({users: users.map(user => user.toObject({getters: true}))})
+}
+
+const signUp = async (req, res, next) => {
+
+    // CODE BEFORE MONGOOSE
+
+    // const errors = validationResult(req);
+
+    // if (!errors.isEmpty()) {
+    //     console.log(errors)
+        
+    //     throw new HttpError("Invalid inputs passed, please check your data.", 422)
+    // }
+
+    // const { name, email, password } = req.body
+
+    // const hasUser = DUMMY_USERS.find(u => u.email === email)
+
+    // if (hasUser) {
+    //     throw new HttpError("There is already an account with that email", 422)
+    // }
+
+    // const createdUser = {
+    //     id: uuid(),
+    //     name,
+    //     email,
+    //     password
+    // }
+
+    // DUMMY_USERS.push(createdUser)
 
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         console.log(errors)
-        
-        throw new HttpError("Invalid inputs passed, please check your data.", 422)
+        return next(
+            new HttpError(
+                "Invalid inputs passed, please check your data.",
+                422
+            )
+        )
     }
 
-    const { name, email, password } = req.body
+    const { name, email, password, places } = req.body
 
-    const hasUser = DUMMY_USERS.find(u => u.email === email)
+    let existingUser
 
-    if (hasUser) {
-        throw new HttpError("There is already an account with that email", 422)
+    try {
+        existingUser = await User.findOne({ email: email })
+    } catch(err) {
+        const error = new HttpError(
+            "Signing up failed, please try again later.",
+            500
+        )
+
+        return next(error)
     }
 
-    const createdUser = {
-        id: uuid(),
+    if (existingUser) {
+        const error = new HttpError(
+            "User exists already, please login instead.",
+            422
+        )
+
+        return next(error)
+    }
+
+    const createdUser = new User({
         name,
         email,
-        password
+        image: "https://picsum.photos/200",
+        password,
+        places
+    })
+
+    try {
+        await createdUser.save()
+    } catch(err) {
+        const error = new HttpError(
+            "Signing up failed, please try again.",
+            500
+        )
+
+        return next(error)
     }
 
-    DUMMY_USERS.push(createdUser)
+    res.status(201).json({ user : createdUser.toObject({ getters: true }) })
+}
 
-    res.status(201).json({ user : createdUser })
-})
+const userLogin = async (req, res, next) => {
 
-const userLogin = ((req, res, next) => {
+    // CODE BEFORE MONGOOSE
+
+    // const { email, password } = req.body
+
+    // const identifiedUser = DUMMY_USERS.find(u => u.email === email)
+    // if (!identifiedUser) {
+    //     return next(
+    //         new HttpError("No user with that email", 401)
+    //     )
+    // } else if (identifiedUser.password !== password) {
+    //     return next(
+    //         new HttpError("Incorrect password", 401)
+    //     )
+    // }
+
+    // res.json({ message : `Logged in as ${identifiedUser.name}` })
+
     const { email, password } = req.body
 
-    const identifiedUser = DUMMY_USERS.find(u => u.email === email)
-    if (!identifiedUser) {
-        throw new HttpError("No user with that email", 401)
-    } else if (identifiedUser.password !== password) {
-        throw new HttpError("Incorrect password", 401)
+    let existingUser
+
+    try {
+        existingUser = await User.findOne({ email: email })
+    } catch (err) {
+        const error = new HttpError(
+            "Logging in failed, please try again",
+            500
+        )
+
+        return next(error)
     }
 
-    res.json({ message : `Logged in as ${identifiedUser.name}` })
-})
+    if (!existingUser || existingUser.password !== password) {
+        const error = new HttpError(
+            "Invalid credentials, could not log you in.",
+            401
+        )
+
+        return next(error)
+    }
+
+    res.json({ message: `Logged in as ${ existingUser.name }`})
+}
 
 exports.getUserList = getUserList
 exports.signUp = signUp
