30f11665307e8706f078a03a8305bd6b56e9f04c
Jordan McGhee <jordanmcghee95@gmail.com>
2022-01-13 21:09:44 -0500

    Updated backend to store files using multer

--- MERN-stack/backend/routes/user-routes.js
+++ MERN-stack/backend/routes/user-routes.js
@@ -2,11 +2,14 @@ const express = require("express")
 const { check } = require("express-validator")
 
 const userControllers = require("../controllers/users-controller")
+const fileUpload = require("../middleware/file-upload")
 const router = express.Router()
 
 router.get("/", userControllers.getUserList)
 
-router.post("/signup",
+router.post(
+    "/signup",
+    fileUpload.single('image'),
     [
         check("name")
         .not(),
