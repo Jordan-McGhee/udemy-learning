73deb01d309172e2204ec694afbbc79758e37767
Jordan McGhee <jordanmcghee95@gmail.com>
2022-01-16 18:08:13 -0500

    Added unhashing for logging in user with passwords

--- MERN-stack/backend/controllers/users-controller.js
+++ MERN-stack/backend/controllers/users-controller.js
@@ -189,10 +189,32 @@ const userLogin = async (req, res, next) => {
     }
 
     // if existingUser is falsey or the password doesn't match the password stored for the queried user, return an error
-    if (!existingUser || existingUser.password !== password) {
+    if (!existingUser) {
         const error = new HttpError(
             "Invalid credentials, could not log you in.",
             401
+        )
+
+        return next(error)
+    }
+
+    let isValidPassword = false
+    
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password)
+    } catch(err) {
+        const error = new HttpError(
+            "Could not log you in, please check your password and try again.",
+            500
+        )
+
+        return next(error)
+    }
+
+    if (!isValidPassword) {
+        const error = new HttpError(
+            "Could not log you in, please check your password and try again.",
+            500
         )
 
         return next(error)
