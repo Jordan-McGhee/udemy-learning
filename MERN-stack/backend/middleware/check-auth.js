204c90e28cb265eb92c91d8b5a96088c38af86b4
Jordan McGhee <jordanmcghee95@gmail.com>
2022-01-19 21:33:41 -0500

    Added middleware function to protect other middleware functions from being accessed by users without a valid token

---
+++ MERN-stack/backend/middleware/check-auth.js
@@ -0,0 +1,26 @@
+const HttpError = require("../models/http-error")
+const jwt = require("jsonwebtoken")
+
+module.exports = (req, res, next) => {
+
+    try{
+        const token = req.headers.authorization.split(" ")[1]
+
+        if (!token) {
+            throw new Error("Authentication failed - check-auth.js")
+        }
+
+        const decodedToken = jwt.verify(token, 'super_secret_dont_share')
+        req.userData = {
+            userID: decodedToken.userID
+        }
+        
+        next()
+
+    } catch(err) {
+        const error = new HttpError("Authentication failed - check-auth.js", 401)
+        return next(error)
+    }
+
+
+}
