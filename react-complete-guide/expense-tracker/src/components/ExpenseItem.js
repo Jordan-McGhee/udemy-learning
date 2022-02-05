c3d89e1d028e6b1af73462a16b3eb89740e063a0
Jordan McGhee <jordanmcghee95@gmail.com>
2022-02-04 19:53:37 -0500

    set up first component to receive data with props

--- react-complete-guide/expense-tracker/src/components/ExpenseItem.js
+++ react-complete-guide/expense-tracker/src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem () {
+function ExpenseItem (props) {
+
     return(
         <div className="expense-item">
 
             <div>
-                Date
+                { props.date.toISOString() }
             </div>
 
             <div className="expense-item__description">
-                <h2>Title</h2>
+                <h2>{ props.title }</h2>
 
                 <div className="expense-item__price">
-                    <h2>Amount</h2>
+                    <h2> $ { props.amount } </h2>
                 </div>
 
             </div>
